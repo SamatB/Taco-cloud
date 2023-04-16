7be4b480d707425605a9c09807c48964871c52e4
samat <samhardy1927@gmail.com>
2023-04-16 17:45:58 +0300

    start

---
+++ src/test/java/ru/beganov/toco/Tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.beganov.toco.Tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
