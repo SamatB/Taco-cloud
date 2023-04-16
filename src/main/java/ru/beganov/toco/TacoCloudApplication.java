260d20b6116deb2d6b329ddde83bf3314895b656
samat <samhardy1927@gmail.com>
2023-04-16 18:50:21 +0300

    Home page works

--- src/main/java/ru/beganov/toco/TacoCloudApplication.java
+++ src/main/java/ru/beganov/toco/TacoCloudApplication.java
@@ -1,4 +1,4 @@
-package ru.beganov.toco.Tacocloud;
+package ru.beganov.toco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ru/beganov/toco/TacoCloudApplicationTests.java
+++ src/test/java/ru/beganov/toco/TacoCloudApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.beganov.toco.Tacocloud;
+package ru.beganov.toco;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
