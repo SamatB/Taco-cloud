3d94bbabf8f1ec6f332d6afa170c940e8143e24e
samat <samhardy1927@gmail.com>
2023-04-19 01:19:58 +0300

    Changed DesignTacoController, IngredientByIdConverter, application.properties

--- src/main/java/ru/beganov/tacos/controller/DesignTacoController.java
+++ src/main/java/ru/beganov/tacos/controller/DesignTacoController.java
@@ -3,43 +3,59 @@ package ru.beganov.tacos.controller;
 import jakarta.validation.Valid;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import ru.beganov.tacos.db.IngredientRepository;
 import ru.beganov.tacos.entity.Ingredient;
 import ru.beganov.tacos.entity.Taco;
 import ru.beganov.tacos.entity.TacoOrder;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Data
+@Controller
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
-    @ModelAttribute
-    public void addAttributeToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
-        );
+    private final IngredientRepository ingredientRepository;
 
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        Iterable<Ingredient> ingredients = ingredientRepository.findAll();
         Ingredient.Type[] types = Ingredient.Type.values();
         for (Ingredient.Type type : types) {
-            model.addAttribute(type.toString().toLowerCase());
-            filterByType(ingredients, type);
+            model.addAttribute(type.toString().toLowerCase(), filterByType((List<Ingredient>) ingredients, type));
         }
+
+//        List<Ingredient> ingredients = Arrays.asList(
+//                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+//                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+//                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+//                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+//                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+//                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+//                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+//                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+//                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+//                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+//        );
+//
+//        Ingredient.Type[] types = Ingredient.Type.values();
+//        for (Ingredient.Type type : types) {
+//            model.addAttribute(type.toString().toLowerCase());
+//            filterByType(ingredients, type);
+//        }
     }
 
     @ModelAttribute(name = "tacoOrder")
