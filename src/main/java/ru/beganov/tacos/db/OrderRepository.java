43cc796c2a2aed4117de44c8377d7ed4a8bbc069
samat <samhardy1927@gmail.com>
2023-04-20 01:02:53 +0300

    @Query

--- src/main/java/ru/beganov/tacos/db/OrderRepository.java
+++ src/main/java/ru/beganov/tacos/db/OrderRepository.java
@@ -1,9 +1,22 @@
 package ru.beganov.tacos.db;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.beganov.tacos.entity.TacoOrder;
+
+import java.util.Date;
+import java.util.List;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
     TacoOrder save(TacoOrder tacoOrder);
+
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
+            String deliveryZip, Date startDate, Date endDate);
+
+//    @Query("FROM TacoOrder o where o.deliveryCity= )
+//    List<TacoOrder> readOrdersDeliveredInSeattle(String seattle);
+
 }
