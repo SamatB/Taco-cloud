6e04c284fb21b94c790565345bd8cf07bc71789f
samat <samhardy1927@gmail.com>
2023-04-27 00:38:10 +0300

    SecurityConfig added

--- src/main/java/ru/beganov/tacos/security/SecurityConfig.java
+++ src/main/java/ru/beganov/tacos/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package ru.beganov.tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,6 +13,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .authorizeRequests()
+                .requestMatchers("/design", "/orders").access("hasRole('USER')")
+                .requestMatchers("/", "/**").access("permitAll()")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and().build();
     }
 
 }
