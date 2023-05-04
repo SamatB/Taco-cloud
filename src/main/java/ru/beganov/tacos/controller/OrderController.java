88f7ad93bea12bba77a0133d315a7d84bce3299e
samat <samhardy1927@gmail.com>
2023-05-04 23:33:06 +0300

    in OrderController added findUsersOrders method

--- src/main/java/ru/beganov/tacos/controller/OrderController.java
+++ src/main/java/ru/beganov/tacos/controller/OrderController.java
@@ -2,8 +2,14 @@ package ru.beganov.tacos.controller;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,9 +24,16 @@ import ru.beganov.tacos.entity.User;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
 
-    private OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
+
+    private int pageSize = 20;
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
 
     public OrderController(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -43,5 +56,13 @@ public class OrderController {
 //        log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(
+            @AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders", orderRepository.findByUserOOrderByPlaceAtDesc(user, pageable));
+        return "orderList";
     }
 }
--- src/main/java/ru/beganov/tacos/db/OrderRepository.java
+++ src/main/java/ru/beganov/tacos/db/OrderRepository.java
@@ -1,11 +1,11 @@
 package ru.beganov.tacos.db;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.beganov.tacos.entity.TacoOrder;
 import ru.beganov.tacos.entity.User;
 
-import java.awt.print.Pageable;
 import java.util.Date;
 import java.util.List;
 
