ca12e06b63aecf48daa8e75c09adf1218ea9703b
samat <samhardy1927@gmail.com>
2023-05-04 23:16:51 +0300

    in OrderRepository added new method

--- src/main/java/ru/beganov/tacos/db/OrderRepository.java
+++ src/main/java/ru/beganov/tacos/db/OrderRepository.java
@@ -3,7 +3,9 @@ package ru.beganov.tacos.db;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.beganov.tacos.entity.TacoOrder;
+import ru.beganov.tacos.entity.User;
 
+import java.awt.print.Pageable;
 import java.util.Date;
 import java.util.List;
 
@@ -15,6 +17,8 @@ public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
     List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
             String deliveryZip, Date startDate, Date endDate);
+
+    List<TacoOrder> findByUserOOrderByPlaceAtDesc(User user, Pageable pageable);
 
 //    @Query("FROM TacoOrder o where o.deliveryCity= )
 //    List<TacoOrder> readOrdersDeliveredInSeattle(String seattle);
