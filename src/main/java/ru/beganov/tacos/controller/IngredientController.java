52ebf19151a347a889506f2733f60c7a21b03d32
samat <samhardy1927@gmail.com>
2023-05-09 00:26:15 +0300

    get, create, delete methods added in IngredientController

--- src/main/java/ru/beganov/tacos/controller/IngredientController.java
+++ src/main/java/ru/beganov/tacos/controller/IngredientController.java
@@ -1,10 +1,10 @@
 package ru.beganov.tacos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.beganov.tacos.db.IngredientRepository;
+import ru.beganov.tacos.entity.Ingredient;
 
 @RestController
 @RequestMapping(path = "/api/ingredients", produces = "application/json")
@@ -18,5 +18,20 @@ public class IngredientController {
         this.ingredientRepository = ingredientRepository;
     }
 
+    @GetMapping
+    public Iterable<Ingredient> getAllIngredients() {
+        return ingredientRepository.findAll();
+    }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ingredient createIngredient(@RequestBody Ingredient ingredient) {
+        return ingredientRepository.save(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteIngredient(@PathVariable("id") String ingredientId) {
+        ingredientRepository.deleteById(ingredientId);
+    }
 }
